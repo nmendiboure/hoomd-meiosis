f13f45bb9a8e76393d5f91eb8bb40c30181b6df7
nmendiboure <nicolas_mendia@yahoo.com>
2024-03-19 12:56:07 +0100

    started to add force definitions

--- hoomd4/new_meiosis_md.py
+++ hoomd4/new_meiosis_md.py
@@ -8,6 +8,7 @@ import numpy as np
 from utils import is_debug
 from polymer.poly import make_polymer
 
+pi = np.pi
 seed = 42
 np.random.seed(seed)
 random.seed(seed)
@@ -69,14 +70,14 @@ if __name__ == "__main__":
 
     n_bonds = n_particles - n_poly
     bonds_group = np.zeros((n_bonds, 2), dtype=int)
-    bonds_types = ["dna-telo", "dna-dna", "dna-dsb"]
+    bonds_types = ["dna-telo", "dna-dna", "dna-dsb", "dsb-dsb"]
     bonds_typeid = np.array(
         [bonds_types.index("dna-telo") if i == 0 or i == size - 2 else bonds_types.index("dna-dna")
          for size in poly_sizes for i in range(size - 1)])
 
     n_angles = n_particles - n_poly * 2
     angles_group = np.zeros((n_angles, 3), dtype=int)
-    angles_types = ["dna-dna-dna"]
+    angles_types = ["dna-dna-dna", "dna-dsb-dna"]
     angles_typeid = np.array([angles_types.index("dna-dna-dna") for size in poly_sizes for i in range(size - 2)])
 
     # Optimizing loops for bonds and angles
@@ -122,4 +123,72 @@ if __name__ == "__main__":
         os.remove(snapshot_save_path)
     with gsd.hoomd.open(name=snapshot_save_path, mode='x') as f:
         f.append(frame)
+
+    """
+    ############################################
+    #####   Initialize the simulation      #####
+    ############################################
+    """
+
+    if mode == 'cpu':
+        device = hoomd.device.CPU(notice_level=notice_level)
+    elif mode == 'gpu':
+        device = hoomd.device.GPU(notice_level=notice_level)
+    else:
+        raise Exception("mode must be 'cpu' or 'gpu'")
+
+    sim = hoomd.Simulation(device=device, seed=seed)
+    sim.create_state_from_gsd(snapshot_save_path)
+    integrator = hoomd.md.Integrator(dt=dt)
+
+    """
+    ############################################
+    #####            Define Forces         #####
+    ############################################
+    """
+
+    # Set up the molecular dynamics simulation
+    # Define bond strength and type
+    harmonic_bonds = hoomd.md.bond.Harmonic()
+    harmonic_bonds.params['dna-dna'] = dict(k=800, r0=1)
+    harmonic_bonds.params['dna-telo'] = dict(k=800, r0=1)
+    harmonic_bonds.params['dsb-dsb'] = dict(k=80, r0=1)
+    integrator.forces.append(harmonic_bonds)
+
+    # Define angle energy and type
+    # k-parameter acting on the persistence length
+    harmonic_angles = hoomd.md.angle.Harmonic()
+    harmonic_angles.params['dna-dna-dna'] = dict(k=persistence_length, t0=pi)
+    integrator.forces.append(harmonic_angles)
+
+    #   Define pairwise interactions
+    telo_group = hoomd.filter.Type(["tel"])
+    group_not_telo = hoomd.filter.SetDifference(f=hoomd.filter.All(), g=telo_group)
+    breaks_group = hoomd.filter.Type(["dsb"])
+
+    cell = hoomd.md.nlist.Cell(buffer=0.4)
+    #   Computes the radially shifted Lennard-Jones pair force on every particle in the simulation state
+    shifted_lj = hoomd.md.pair.ForceShiftedLJ(nlist=cell, default_r_cut=1.5)
+    shifted_lj.params[('dna', 'dna')] = dict(epsilon=1.0, sigma=1.0)
+    shifted_lj.params[('dna', 'dsb')] = dict(epsilon=1.0, sigma=1.0)
+    shifted_lj.params[('dna', 'tel')] = dict(epsilon=1.0, sigma=1.0)
+    shifted_lj.params[('tel', 'tel')] = dict(epsilon=1.0, sigma=1.0)
+    shifted_lj.params[('tel', 'dsb')] = dict(epsilon=1.0, sigma=1.0)
+    shifted_lj.params[('dsb', 'dsb')] = dict(epsilon=1.0, sigma=1.0)
+
+    shifted_lj.r_cut[('dna', 'dna')] = 2**(1.0 / 6.0)
+    shifted_lj.r_cut[('dna', 'dsb')] = 2**(1.0 / 6.0)
+    shifted_lj.r_cut[('dna', 'tel')] = 2**(1.0 / 6.0)
+    shifted_lj.r_cut[('tel', 'tel')] = 2**(1.0 / 6.0)
+    shifted_lj.r_cut[('tel', 'dsb')] = 2**(1.0 / 6.0)
+    shifted_lj.r_cut[('dsb', 'dsb')] = 2**(1.0 / 6.0)
+
+    sphere = hoomd.wall.Sphere(radius=radius, origin=(0., 0., 0.), inside=True)
+    walls = [sphere]
+    shifted_lj_wall = hoomd.md.external.wall.ForceShiftedLJ(walls)
+    # repulsive interaction because cut at the minimum value
+    shifted_lj_wall.params[particles_types] = dict(epsilon=1, sigma=2, r_cut=2**(1 / 6))
+    shifted_lj_wall.params['tel'] = dict(epsilon=2, sigma=1, r_extrap=0, r_cut=3)
+    integrator.forces.append(shifted_lj)
+    integrator.forces.append(shifted_lj_wall)
 
